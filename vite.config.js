ca0a5c2f9f890659daab421fa12ec3e30137416f
gianttelepathicsquid <kevinxrp@gmail.com>
2024-10-25 12:38:26 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   build: {
     sourcemap: true,
     rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'lucide-react']
